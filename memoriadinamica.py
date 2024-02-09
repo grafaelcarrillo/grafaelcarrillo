92e47984987f3a309dea31b94533791dcfe49153
Gustavo Carrillo <grafaelcarrillo@gmail.com>
2024-02-09 08:43:46 -0600

    Add files via upload

---
+++ memoriadinamica.py
@@ -0,0 +1,18 @@
+frutas= []
+   
+frutas.append("Mango")
+frutas.append("Manzana")
+frutas.append("Banana")
+frutas.append("Uvas")
+      
+print(frutas)
+      
+del frutas[0]
+del frutas[1]
+      
+frutas.append("sandia")
+       
+print(frutas)
+
+
+
---
+++ memoriaestatica.py
@@ -0,0 +1,9 @@
+vector = []
+for i in range(5):
+    dato = int(input(f"Introduce la calificación para la posición {i}: "))
+   
+    vector.append(dato)
+
+print("Las calificaciones ingresadas son:", vector)
+
+
